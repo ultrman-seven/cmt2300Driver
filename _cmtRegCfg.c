67b6c9950746f5333416a7eaffbfa40fc0968da0
Leslie <1813180652@qq.com>
2026-09-19 23:44:08 +0800

    update

---
+++ _cmtRegCfg.c
@@ -0,0 +1,5 @@
+#include "stdint.h"
+
+const uint16_t cmtRegCfgDataLen = 0;
+const uint16_t cmtRegCfgData[] = {
+    0x00};
--- cmt2300.c
+++ cmt2300.c
@@ -39,11 +39,18 @@ typedef struct
     uint8_t isRx;
 
     uint8_t fsm;
+    uint8_t targetFsm;
 
     cmt2300Cfg_t cfg;
     void (*cbk)(void *);
     void *arg;
     uint8_t regBuf[4];
+    struct
+    {
+        uint8_t *buf;
+        uint32_t len;
+    } rx, tx;
+
 } cmt2300Handle_t;
 
 #include "cmt2300.h"
@@ -69,11 +76,12 @@ typedef struct
 enum
 {
     Cmt2300Fsm_Boot = 0,
+    Cmt2300Fsm_Init,
     Cmt2300Fsm_DelayAfterCsEn,
     Cmt2300Fsm_DelayBeforeCsDis,
     Cmt2300Fsm_DelayAfterFifo,
-    Cmt2300Fsm_,
-    Cmt2300Fsm_,
+    Cmt2300Fsm_Wait4Spi,
+    Cmt2300Fsm_FifoWrite,
     Cmt2300Fsm_,
     Cmt2300Fsm_Idle
 };
@@ -92,6 +100,10 @@ enum
 //     Cmt2300_SpiFsm_complete3UsDelay,
 //     Cmt2300_SpiFsm_,
 // };
+
+extern const uint16_t cmtRegCfgDataLen;
+extern const uint16_t cmtRegCfgData[];
+
 void cmt2300OnIntCbk(cmt2300Handle_t *handle, uint8_t intId)
 {
 }
@@ -107,12 +119,32 @@ static void cmt2300OnSpiTxCpltCbk(void *handle)
 
 #define cmt2300SpiTx(_handle, _buf, _len) (_handle)->cfg.api.spiWriteReadAsync(Cmt2300_SpiDir_Tx, (_buf), (_len), cmt2300OnSpiTxCpltCbk, (_handle))
 
+void cmt2300Boot(cmt2300Handle_t *handle, cmt2300Cfg_t *cfg)
+{
+    handle->fsm = Cmt2300Fsm_Boot;
+}
+
 void cmt2300InitAsync(cmt2300Handle_t *handle, cmt2300Cfg_t *cfg, void (*cbk)(void *), void *arg)
 {
     handle->busBusy = 0;
+    handle->cbk = cbk;
+    handle->arg = arg;
+    handle->fsm = Cmt2300Fsm_Init;
+    handle->targetFsm = Cmt2300Fsm_Init;
 }
 
 void cmt2300ConfigAsync(cmt2300Handle_t *handle, cmt2300Cfg_t *cfg, void (*cbk)(void *), void *arg)
+{
+}
+
+#define Cmt2300Mode_Rx 1
+#define Cmt2300Mode_Tx 0
+
+void cmt2300SetModeAsync(cmt2300Handle_t *handle, uint8_t mode, void (*cbk)(void *), void *arg)
+{
+}
+
+void cmt2300TransmitAsync(cmt2300Handle_t *handle, uint8_t *buf, uint32_t len, void (*cbk)(void *), void *arg)
 {
 }
 
@@ -121,6 +153,59 @@ void cmt2300Loop(cmt2300Handle_t *handle)
     switch (handle->fsm)
     {
     case Cmt2300Fsm_Boot:
+        break;
+    case Cmt2300Fsm_Init:
+        if (handle->busBusy)
+            break;
+        handle->cfg.api.gpioWrite(Cmt2300_PinId_SpiCs, Cmt2300_PinLevel_Low);
+        handle->cfg.api.usTimerCtrl(Cmt2300_TimerCtrlState_RstAndStart);
+        handle->fsm = Cmt2300Fsm_DelayAfterCsEn;
+        break;
+    case Cmt2300Fsm_DelayAfterCsEn:
+        if (Cmt2300Fsm_FifoWrite == handle->targetFsm)
+        {
+            if (handle->cfg.api.usTimerGetCnt() <= Cmt2300DelayTime_us_FifoAftCsEn)
+                break;
+            handle->cfg.api.spiWriteReadAsync(Cmt2300_SpiDir_Tx, handle->tx.buf, 1, cmt2300OnSpiTxCpltCbk, handle);
+            ++(handle->tx.buf);
+            --(handle->tx.len);
+        }
+        else
+        {
+            if (handle->cfg.api.usTimerGetCnt() <= Cmt2300DelayTime_us_SpiAftCsEn)
+                break;
+            handle->cfg.api.spiWriteReadAsync(Cmt2300_SpiDir_Tx, handle->regBuf, 2, cmt2300OnSpiTxCpltCbk, handle);
+        }
+        handle->fsm = Cmt2300Fsm_Wait4Spi;
+        break;
+    case Cmt2300Fsm_DelayBeforeCsDis:
+        if (Cmt2300Fsm_FifoWrite == handle->targetFsm)
+        {
+            if (handle->cfg.api.usTimerGetCnt() <= Cmt2300DelayTime_us_FifoBfCsDis)
+                break;
+            handle->cfg.api.gpioWrite(Cmt2300_PinId_FifoCs, Cmt2300_PinLevel_High);
+            handle->fsm = Cmt2300Fsm_DelayAfterFifo;
+            handle->cfg.api.usTimerCtrl(Cmt2300_TimerCtrlState_RstAndStart);
+        }
+        else
+        {
+        }
+        break;
+    case Cmt2300Fsm_DelayAfterFifo:
+        if (handle->cfg.api.usTimerGetCnt() <= Cmt2300DelayTime_us_FifoAftCsDis)
+            break;
+        if (handle->tx.len)
+        {
+            handle->cfg.api.gpioWrite(Cmt2300_PinId_FifoCs, Cmt2300_PinLevel_Low);
+            handle->fsm = Cmt2300Fsm_DelayBeforeCsDis;
+            handle->cfg.api.usTimerCtrl(Cmt2300_TimerCtrlState_RstAndStart);
+        }
+        break;
+    case Cmt2300Fsm_Wait4Spi:
+        if (handle->busBusy)
+            break;
+        handle->fsm = Cmt2300Fsm_DelayBeforeCsDis;
+        handle->cfg.api.usTimerCtrl(Cmt2300_TimerCtrlState_RstAndStart);
         break;
     case Cmt2300Fsm_:
         break;
